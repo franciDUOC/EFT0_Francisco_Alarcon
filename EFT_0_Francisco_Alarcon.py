a615fffefca573295c335f597c4ad3c0519c6089
franciDUOC <franci.alarconc@duocuc.cl>
2026-07-07 11:58:49 -0400

    Commit con funciones internas, pendiente funciones ejecutar

--- EFT_0_Francisco_Alarcon.py
+++ EFT_0_Francisco_Alarcon.py
@@ -28,9 +28,11 @@ def leer_opcion():
 #Validaciones
 
 def validar_texto(valor):
+#Funcion devuelve bool
     return valor.strip() != ""
 
 def leer_entero(mensaje):
+#Funcion devuelve el numero
     while True:
         try:
             numero = int(input(mensaje))
@@ -39,6 +41,7 @@ def leer_entero(mensaje):
             print("ERROR! Debe ingresar un numero entero.")
 
 def leer_texto_no_vacio(mensaje):
+#Funcion devuelve str
     while True:
         texto = input(mensaje)
         if validar_texto:
@@ -47,41 +50,100 @@ def leer_texto_no_vacio(mensaje):
             print("ERROR! Dato ingresado no puede estar vacio.")
 
 def validar_precio(precio):
+#Funcion devuelve bool
     return precio >= 0
 
 def validar_disponible(disponible):
+#Funcion devuelve bool
     disponible = disponible.lower()
     return disponible == "s" or disponible == "n"
 
 def validar_stock(stock):
+#Funcion devuelve bool
     return stock >= 0
 
 def validar_vendidos(vendidos):
+#Funcion devuelve bool
     return vendidos >= 0
 
 def validar_codigo_nuevo (codigo, productos, inventario):
+#Funcion devuelve bool
     codigo = codigo.lower()
     return codigo != "" and codigo not in productos and codigo not in inventario
 
 #---------------------------------------------------------------------------------
 
+# Esqueleto de las opciones del menu
+
 def stock_categoria(categoria, productos, inventario):
-    pass
+#Funcion devuelve el valor del stock
+    total = 0
+    
+    for codigo in productos:
+        datos_producto = productos[codigo]
+        producto_categoria = datos_producto[1]
+        if producto_categoria.lower() == categoria.lower():
+            if codigo in inventario:
+                stock = inventario[codigo][0]
+                total_stock += stock
+
+    return total_stock
 
 def buscar_precio(precio_min, precio_max, productos, inventario):
-    pass
+#Funcion devuelve lista con productos (nombre -- codigo) en el rango precio minimo y maximo
+    resultados = []
+    for codigo in productos:
+        precio = productos[codigo][2]
+        stock = inventario[codigo][0]
+        if precio >= precio_min and precio <= precio_max and stock != 0:
+            nombre = productos[codigo][0]
+            resultados.append(nombre + "--" + codigo)
+    resultados.sort()
+    return resultados
 
 def buscar_codigo(codigo, productos, inventario):
-    pass
+#Funcion devuelve bool si codigo se encuentra en productos e inventario
+    codigo = codigo.upper()
+    if codigo in productos and codigo in inventario:
+        return True
+    return False
 
 def actualizar_precio(codigo, nuevo_precio, productos, inventario):
-    pass
+#Funcion devuelve bool si buscar codigo es True, actualiza el precio
+    codigo = codigo.upper()
+    if buscar_codigo(codigo, productos, inventario):
+        productos[codigo][2] = nuevo_precio
+        return True
+    return False
 
 def agregar_producto(codigo, nombre, categoria, precio, disponible, stock, vendidos, productos, inventario):
-    pass
+#Funcion devuelve bool si logra registrar nuevo producto
+    codigo = codigo.upper()
+    nombre = nombre.capitalize()
+    categoria = categoria.capitalize()
+    
+    if not validar_codigo_nuevo:
+        return False
+    
+    if disponible == "s":
+        disponible_bool = True
+    else:
+        disponible_bool = False
+    
+    productos[codigo] = [nombre,categoria,precio,disponible_bool]
+    inventario[codigo] = [stock,vendidos]
+    return True
 
 def eliminar_producto(codigo, productos, inventario):
-    pass
+    codigo = codigo.upper()
+    
+    if buscar_codigo(codigo, productos, inventario):
+        del productos[codigo]
+        del inventario[codigo]
+        return True
+    return False
+
+#--------------------------------------------------------------------------------
 
 def ejecutar_stock_categoria(productos, inventario):
     pass
